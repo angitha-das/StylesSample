dc9289baeff7bc09f86ab593cbd8e866b2bc60a5
angitha <angitha.das@cabotsolutions.com>
2017-04-01 20:50:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/angithadas/stylessample/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.angithadas.stylessample;
+
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.lang.reflect.Field;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
